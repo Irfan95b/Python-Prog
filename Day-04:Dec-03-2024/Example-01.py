ae780c602aa9de8d70b9efee59e397e051c6e5f4
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-18 08:32:09 +0000

    Example progs to use {} and .format()

---
+++ Day-04:Dec-03-2024/Example-01.py
@@ -0,0 +1,16 @@
+
+'''
+ Write a python program to print using replacement operator --> {} and .format() function.
+
+    {} --> replacement operator --> (used inside the strings)
+    
+    format() function --> used to provide the value or data from the specified variables or identifiers
+                          with which replacement operator to be replaced.
+
+'''
+
+a = 11
+b = 3
+
+print("sum of {} and {} is {}".format(a,b,a+b))
+
---
+++ Day-04:Dec-03-2024/Example-02.py
@@ -0,0 +1,17 @@
+
+# Write a program to Display Proper messages along with Data using "{} and .format()"
+
+
+a = 34
+b = 10
+
+print("\n\n\t********** Given values for calculation are {} and {} **********\n".format(a,b))
+
+print("\t\t 1. Addition of {} and {} is {}".format(a,b,a+b))
+print("\t\t 2. Subsraction of {} and {} is {}".format(a,b,a-b))
+print("\t\t 3. Multiplication of {} and {} is {}".format(a,b,a*b))
+print("\t\t 4. Divisor of {} and {} is {}".format(a,b,a/b))
+print("\t\t 5. Remainder if {} and {} is {}".format(a,b,a%b))
+
+print("\n\n\n")
+
