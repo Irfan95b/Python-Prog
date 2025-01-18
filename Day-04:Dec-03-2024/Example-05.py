c47d87cfd51db437deedc8ab18da3f05226bb72b
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-18 12:22:11 +0000

    Program to Print Reserved KeyWords in Python

---
+++ Day-04:Dec-03-2024/Example-05.py
@@ -0,0 +1,11 @@
+
+# Write a python program to print Reserved KeyWords in python.
+
+
+
+
+import keyword                      # imports Kewwords library
+
+KeyWords = keyword.kwlist           # Assignes all the keywords list to KeyWords Variable
+
+print(KeyWords)                     # Prints all the Reserved Keywords in python
