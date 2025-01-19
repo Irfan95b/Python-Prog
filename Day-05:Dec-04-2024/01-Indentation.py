139441893087483f434c9a76072b09b91bd4a2da
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-19 10:05:45 +0000

    Example for Indentation

---
+++ Day-05:Dec-04-2024/01-Indentation.py
@@ -0,0 +1,20 @@
+
+'''
+ Indentation in python
+ ---------------------
+    A groupe of statements (Body, Block of Code, Defenition) with equal number of spaces from left.
+
+    Python supports Indetation for (Body, Block of Code, Defenition) But not like other prog lang.
+
+    group of code must be with the equal no of spaces from left
+
+'''
+
+Set_Condition = False
+
+if Set_Condition:   # This is called Header
+    print('True')                       # This is called Suite
+    print("Printing from True Block")   # This is called Suite
+else:   #Header
+    print("False")  #Suite
+    print("Printing from the Else Block")   #Suite
