25955a150b4ee4f8bfa2a531d7d78d50aa08bfe4
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-19 10:07:43 +0000

    Example for using single line Multi statements in python

---
+++ Day-05:Dec-04-2024/02-Single-Line-Statements.py
@@ -0,0 +1,9 @@
+
+'''
+ In Python programming we can write multiple statements in single line
+
+ After completion of each statement add semi-colon(;) 
+
+'''
+
+a = 10; b = 20; Sum = a + b; print("Sum of {} and {} is {}".format(a,b,Sum))
