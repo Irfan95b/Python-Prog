a0e0ef48e162777eec8098a88de557d548b1110c
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-21 09:54:47 +0000

    complex data type in python

---
+++ Day-06:Dec-05-2024/04-complex-data-type.py
@@ -0,0 +1,10 @@
+'''
+    Complex Datatypes are used in scientific application.
+    in mathematics complex numbers are given with 'i' but in python we use 'j'
+
+'''
+
+a = 23+5j
+
+print(a)
+print(type(a))
