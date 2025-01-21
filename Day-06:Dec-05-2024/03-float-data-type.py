2c713ef8602dfcc8429d9555596ed97543d574f5
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-21 09:54:20 +0000

    float data type in python

---
+++ Day-06:Dec-05-2024/03-float-data-type.py
@@ -0,0 +1,17 @@
+'''
+    float data-type represents value with decimal point values
+
+
+'''
+
+a = 153.24 # Standerd Notation.
+
+print(a)
+
+a = 1.5e3 # Scientific Notation.
+'''
+                e3 ==> (value) x 10^3, i.e, 1.5x10^3
+                ==> 1500.0
+'''
+print(a)
+
