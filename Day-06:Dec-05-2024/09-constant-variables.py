48a283b993e6ccc41521304a1e3c959983b0a23b
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-21 09:57:49 +0000

    constant variables in python

---
+++ Day-06:Dec-05-2024/09-constant-variables.py
@@ -0,0 +1,21 @@
+
+'''
+    Constant Variables
+    -------------------
+        constant variables have fixed value through out the program and cannot be changed.
+
+        Python does not support constant values in its variables.
+        However we can achive this by using Block Letter variables 
+            (Ex:- CONT_VAR = 120).
+            But still we can modify these values, It is just to set som standerd.
+        
+'''
+
+PI = 3.14
+print(PI)
+
+MAX = 100  # But still we can change these values.
+MIN = 0
+
+PI = 3.20
+print(PI)
