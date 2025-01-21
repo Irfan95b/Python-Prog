ddfc7f0968e77d729436af8623c9a186f5c59a5a
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-01-21 09:57:10 +0000

    None data type in python

---
+++ Day-06:Dec-05-2024/08-None-data-type.py
@@ -0,0 +1,21 @@
+'''
+    None Data Type
+    --------------
+        None means Nothing or no value data type
+
+'''
+
+a = None
+print(a)
+print(id(a))
+print(type(a))
+
+a = 20
+print(a)
+print(id(a))
+print(type(a))
+
+a = None
+print(a)
+print(id(a))
+print(type(a))
