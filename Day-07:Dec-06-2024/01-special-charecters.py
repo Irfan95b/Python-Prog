bb05efc993a8ed0ae136b26373c0e8359482c84c
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-02-05 09:34:39 +0000

    Examples for special charecters in python

---
+++ Day-07:Dec-06-2024/01-special-charecters.py
@@ -0,0 +1,51 @@
+'''
+
+  ->  Special charecters can also be called as "escape sequeance charecters"
+  ->  They are special charecters and combination of two charecters.
+  ->  special meaning and special usage in program.
+
+'''
+
+
+# 1. new line charecter (\n) :
+#   Moves the courcer to the next line.
+
+print("Hello\nWelcome\nto\nPython\nProgramming")
+
+
+print("\n-------------------------------------------------------\n")
+
+# 2. tab space charecter (\t) :
+# Moves courcer with the 8 spaces forword or one tab in the same line.
+
+print("Hello\n\tWelcome\nto\n\tPython\nProgramming\t")
+
+print("\n-------------------------------------------------------\n")
+
+# 3. back space charecter (\b):
+# Moves courcer to one space back in the same line.
+
+
+print("Hello Welcome to Python Programming")
+print("Hello \bWelcome \bto \bPython \bProgramming")
+print("Hello\bWelcome\bto\bPython\bProgramming")
+
+print("\n-------------------------------------------------------\n")
+
+# 4. carrieage or return charecter (\r)
+# Takes the courser the begining of the same line.
+
+print("Hello Welcome to Python \rProgramming")
+
+
+print("\n-------------------------------------------------------\n")
+
+# 5. To use back slash in backword in string printing we use bouble back slach (\\)
+
+print("Hello \\\\ wellcome {data}  // to python programming")
+
+print("\n-------------------------------------------------------\n")
+
+# 6. Example for using sinle and double cotes ( ' & " )
+
+print("Hello \'welcome\' back to \"python\" programming")
