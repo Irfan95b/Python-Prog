aea6ab2b43073dd047788965740e5c8ee800168a
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-02-07 10:38:09 +0000

    Theory of controle structures or flow controle

---
+++ Day-09:Dec-09-2024/03-Controle-Structures.py
@@ -0,0 +1,45 @@
+'''
+    Controle Structures:-
+        Control-Structures also known as Flow-Controls in Python
+        Control means <<Condition>>
+            Ex:-
+                (a<b):
+
+                a = 234
+                (a == 234):
+
+        Structure means indented-block-of-code
+            Ex:-
+                (condition):
+                    stmt 1;
+                    stmt 2;
+                    '           --> suite (indented block of code)
+                    '
+                    '
+                    stmt n;
+
+        Defenition:
+            Executing an indented-block-of-code, based on <<condition>> is known as Control-Structure or Flow-Controls
+
+        In Python, we dont have blocks with {} parethesis.
+        Here we have indented-block-of-code
+        single/multiple stmts with equal spaces from left is called suite or indented block of code.
+        Flow-control means the order in which statements are executed in program.
+
+    CS/FC classification :-
+    --------------------
+                                    Controle Structure / Flow Controle
+                                                    |
+         |''''''''''''''''''''''''''''''''''''''''''|''''''''''''''''''''''''''''''''''''''''''''|
+   A) Conditional stmts                      b) Iterative stmts                         c) Transfer stmts
+            or                                      or                                          or
+      (Branching-Stmts)                         Looping Statments                           Jumps-Stmts
+
+      Ex:-                                      Ex:-                                        Ex:-
+        simple if                                   for                                         break
+        multiple if                                 while                                       continue
+        if-else                                     nested loops                                pass
+        nested if
+        if-elif-else
+
+'''
