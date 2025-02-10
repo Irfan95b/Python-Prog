b479d6a176034512fad246d56bb3f8c4e59c26d6
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-02-10 15:01:16 +0000

    Program to see eligibility for voting

---
+++ Day-10:Dec-11-2014/06-voter-age.py
@@ -0,0 +1,11 @@
+
+# WAP to accept age of a person as input & display "Eligible for Voting or not"
+
+age = int(input("Enter any age : "))
+
+if(age >= 18):
+    print("You are eligible for voting")
+else:
+    print("You are not eligible for voting")
+
+
