3e4e312b5941e89a64790dd4116d19bbbe89e78b
EC2 Default User <ec2-user@amazonlinux.onprem>
2025-02-10 15:00:11 +0000

    even odd program using if

---
+++ Day-10:Dec-11-2014/04-even-odd.py
@@ -0,0 +1,13 @@
+
+#(Program to demo with Multiple-if stmts with Even,Odd,Neither of them)
+
+num = int(input("Enter any integer number : "))
+
+if(num == 0):
+    print("Please Enter any number greater than zero");
+if(num%2 == 0):
+    print("Given number is even");
+if(num%2 == 1):
+    print("Given number is odd");
+
+print("End of program\n");
